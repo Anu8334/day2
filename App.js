bd13ad1fb71254d336d2efbe1c78aadd1d4150ee
Anushka Roy <royanushka145@gmail.com>
2024-12-29 09:40:31 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "./index.css"
+
+// const div1 = React.createElement(
+//   "div",
+//   { id: "parent" },[
+//   React.createElement("div", { id: "child1" }, [
+//     React.createElement("h1", {}, "This is a simple header1🤩"),
+//     React.createElement("h1", {}, "This is a simple header2..."),
+//   ]),
+//   React.createElement("div", { id: "child2" }, [
+//     React.createElement("h1", {}, "This is a simple header1🤩"),
+//     React.createElement("h1", {}, "This is a simple header2😈"),
+//   ])
+// ]);
+
+// const div1=<h1>"This is a simple header1🤩"</h1>;//js er object ba react er element
+// const Div1= () =>{
+//   return (<h1>"This is a simple header1🤩"</h1>);
+// };
+//component er name should start with capital letter. 
+
+const Div1= () =>{
+  return (
+    <div className= "parent">
+      <div className="child">
+        <h1 className="header">"This is a simple header1🤩"</h1>
+      </div>
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Div1/>);
---
+++ Test.js
@@ -0,0 +1,15 @@
+test1();
+// test2();
+// test3();
+console.log(x);
+
+var x=5;
+function test1(){
+    console.log("function 1");
+}
+const test2 = function(){
+    console.log("function 2");
+}
+const test3 = () => {
+    console.log("function 3");
+}
